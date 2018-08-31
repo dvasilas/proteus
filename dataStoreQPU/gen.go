664e8a0a923d361289d19c295f28238915b974a9
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2018-08-31 17:23:20 +0200

    s3 cloud server: index functionality

--- dataStoreQPU/gen.go
+++ dataStoreQPU/gen.go
@@ -1,4 +1,5 @@
 //go:generate protoc --proto_path=./dsqpupb --proto_path=../qpuUtilspb --go_out=plugins=grpc:$GOPATH/src ./dsqpupb/dsqpu.proto
+//go:generate protoc --proto_path=./s3pb --go_out=plugins=grpc:$GOPATH/src ./s3pb/s3oplog.proto
 //go:generate mockgen -destination=client/mocks/dsqpu_mock.go github.com/dimitriosvasilas/modqp/dataStoreQPU/dsqpupb DataStoreQPUClient,DataStoreQPU_GetSnapshotClient
 
 package gen
