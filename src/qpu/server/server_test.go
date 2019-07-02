1c6aa2d9730a39a9a841af70aac8dfc8f0f493d6
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2019-07-02 15:51:11 +0200

    make end-to-end tests more generic

--- src/qpu/server/server_test.go
+++ src/qpu/server/server_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/dvasilas/proteus/src/config"
 	pbQPU "github.com/dvasilas/proteus/src/protos/qpu"
 	pbUtils "github.com/dvasilas/proteus/src/protos/utils"
+	"github.com/dvasilas/proteus/src/qpu/cache"
 	"github.com/dvasilas/proteus/src/qpu/client"
 	"github.com/dvasilas/proteus/src/qpu/datastore_driver"
 	"github.com/dvasilas/proteus/src/qpu/filter"
@@ -20,49 +21,194 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-type testDeployment struct {
-	datastoreDriver *datastoredriver.DriverQPU
-	filter          *filter.FQPU
-	dsDriverConf    *config.Config
-	filterConf      *config.Config
+type deployment struct {
+	config          []*config.Config
+	datastoredriver *datastoredriver.DriverQPU
+	servers         []*grpc.Server
 }
 
 type testScenario struct {
-	endpoint       string
-	ops            []*pbUtils.LogOperation
-	query          []*pbUtils.AttributePredicate
-	assertObjectID string
+	deployment deployment
+	endpoint   string
+	plan       queryPlan
 }
 
-var depl = testDeployment{
-	dsDriverConf: &config.Config{
+type queryPlan []struct {
+	query          []*pbUtils.AttributePredicate
+	assertResponse string
+}
+
+type datastoreDeployment struct {
+	config          *config.Config
+	datastoredriver *datastoredriver.DriverQPU
+	ops             []*pbUtils.LogOperation
+}
+
+var singleDatastore = datastoreDeployment{
+	config: &config.Config{
 		QpuType: pbQPU.ConfigResponse_DBDRIVER,
 		Port:    "50150",
 	},
-	filterConf: &config.Config{
-		QpuType:     pbQPU.ConfigResponse_FILTER,
-		Port:        "50250",
-		Connections: []string{"localhost:50150"},
+	ops: []*pbUtils.LogOperation{
+		protoutils.LogOperation(
+			"object1",
+			"mock_bucket",
+			pbUtils.LogOperation_S3OBJECT,
+			protoutils.Vectorclock(map[string]uint64{"50150": uint64(time.Now().Nanosecond())}),
+			protoutils.PayloadDelta(nil,
+				protoutils.ObjectState(
+					[]*pbUtils.Attribute{
+						protoutils.Attribute("attr", pbUtils.Attribute_S3TAGINT, protoutils.ValueInt(42)),
+					},
+				),
+			),
+		),
 	},
+}
+
+var tests = []testScenario{
+	testScenario{
+		deployment: deployment{
+			config: []*config.Config{
+				&config.Config{
+					QpuType:     pbQPU.ConfigResponse_FILTER,
+					Port:        "50250",
+					Connections: []string{"localhost:50150"},
+				},
+			},
+		},
+		endpoint: "localhost:50250",
+		plan: queryPlan{
+			{
+				query: []*pbUtils.AttributePredicate{
+					protoutils.AttributePredicate(protoutils.Attribute("attr", pbUtils.Attribute_S3TAGINT, nil),
+						protoutils.ValueInt(40),
+						protoutils.ValueInt(50),
+					),
+				},
+				assertResponse: "object1",
+			},
+		},
+	},
+	{
+		deployment: deployment{
+			config: []*config.Config{
+				&config.Config{
+					QpuType:     pbQPU.ConfigResponse_FILTER,
+					Port:        "50250",
+					Connections: []string{"localhost:50150"},
+				},
+				&config.Config{
+					QpuType:     pbQPU.ConfigResponse_CACHE,
+					Port:        "50451",
+					Connections: []string{"localhost:50250"},
+				},
+			},
+		},
+		endpoint: "localhost:50451",
+		plan: queryPlan{
+			{
+				query: []*pbUtils.AttributePredicate{
+					protoutils.AttributePredicate(protoutils.Attribute("attr", pbUtils.Attribute_S3TAGINT, nil),
+						protoutils.ValueInt(40),
+						protoutils.ValueInt(50),
+					),
+				},
+				assertResponse: "object1",
+			},
+		},
+	},
+}
+
+func TestEndToEnd(t *testing.T) {
+	deployPopulateDatastore(singleDatastore)
+
+	for _, tt := range tests {
+		deploy(&tt.deployment)
+		doTest(t, tt)
+		for _, s := range tt.deployment.servers {
+			s.Stop()
+		}
+	}
 }
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func deploy(conf *config.Config, api QPUAPI) <-chan int {
+func deployPopulateDatastore(depl datastoreDeployment) {
+	depl.config.DatastoreConfig.Type = config.MOCK
+	dsqpu, err := datastoredriver.QPU(depl.config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		server := QPUServer{config: depl.config, api: dsqpu}
+		s := grpc.NewServer()
+		pbQPU.RegisterQPUServer(s, &server)
+		reflection.Register(s)
+		lis, err := net.Listen("tcp", ":"+depl.config.Port)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Serve(lis)
+	}()
+
+	for _, op := range depl.ops {
+		dsqpu.Op(op)
+	}
+}
+
+func deploy(depl *deployment) {
+	createQPU(depl.config, depl)
+}
+
+func createQPU(conf []*config.Config, depl *deployment) {
+	if len(conf) == 0 {
+		return
+	}
+	var qpu QPUAPI
+	var err error
+	switch conf[0].QpuType {
+	case pbQPU.ConfigResponse_DBDRIVER:
+		conf[0].DatastoreConfig.Type = config.MOCK
+		dsqpu, err := datastoredriver.QPU(conf[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		depl.datastoredriver = dsqpu
+		qpu = dsqpu
+	case pbQPU.ConfigResponse_FILTER:
+		qpu, err = filter.QPU(conf[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+	case pbQPU.ConfigResponse_CACHE:
+		conf[0].CacheConfig.Size = 10
+		qpu, err = cache.QPU(conf[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	wait := startServer(conf[0], qpu, depl)
+	<-wait
+	createQPU(conf[1:], depl)
+}
+
+func startServer(conf *config.Config, api QPUAPI, depl *deployment) <-chan int {
 	wait := make(chan int)
 	go func() {
-		serverDS := QPUServer{config: conf, api: api}
-		sDS := grpc.NewServer()
-		pbQPU.RegisterQPUServer(sDS, &serverDS)
-		reflection.Register(sDS)
+		server := QPUServer{config: conf, api: api}
+		s := grpc.NewServer()
+		pbQPU.RegisterQPUServer(s, &server)
+		reflection.Register(s)
 		lis, err := net.Listen("tcp", ":"+conf.Port)
 		if err != nil {
 			log.Fatal(err)
 		}
+		depl.servers = append(depl.servers, s)
 		wait <- 0
-		sDS.Serve(lis)
+		s.Serve(lis)
 	}()
 	return wait
 }
@@ -73,73 +219,22 @@ func doTest(t *testing.T, test testScenario) {
 		t.Error(err)
 	}
 
-	for _, op := range test.ops {
-		depl.datastoreDriver.Op(op)
-	}
-
-	stream, _, err := client.Query(
-		test.query,
-		protoutils.SnapshotTimePredicate(
-			protoutils.SnapshotTime(pbUtils.SnapshotTime_ZERO, nil),
-			protoutils.SnapshotTime(pbUtils.SnapshotTime_LATEST, nil),
-		),
-		false, false,
-	)
-	if err != nil {
-		t.Error(err)
-	}
-	streamRec, err := stream.Recv()
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, test.assertObjectID, streamRec.GetLogOp().GetObjectId(), "")
-}
-
-func TestEtE_F(t *testing.T) {
-	var err error
-
-	depl.dsDriverConf.DatastoreConfig.Type = config.MOCK
-	depl.datastoreDriver, err = datastoredriver.QPU(depl.dsDriverConf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	wait := deploy(depl.dsDriverConf, depl.datastoreDriver)
-	<-wait
-
-	depl.filter, err = filter.QPU(depl.filterConf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	wait = deploy(depl.filterConf, depl.filter)
-	<-wait
-
-	doTest(t, testF)
-
-}
-
-var testF = testScenario{
-	endpoint: "localhost:50250",
-	ops: []*pbUtils.LogOperation{
-		protoutils.LogOperation(
-			"object1",
-			"mock_bucket",
-			pbUtils.LogOperation_S3OBJECT,
-			protoutils.Vectorclock(map[string]uint64{depl.dsDriverConf.Port: uint64(time.Now().Nanosecond())}),
-			protoutils.PayloadDelta(nil,
-				protoutils.ObjectState(
-					[]*pbUtils.Attribute{
-						protoutils.Attribute("attr", pbUtils.Attribute_S3TAGINT, protoutils.ValueInt(42)),
-					},
-				),
+	for _, q := range test.plan {
+		stream, _, err := client.Query(
+			q.query,
+			protoutils.SnapshotTimePredicate(
+				protoutils.SnapshotTime(pbUtils.SnapshotTime_ZERO, nil),
+				protoutils.SnapshotTime(pbUtils.SnapshotTime_LATEST, nil),
 			),
-		),
-	},
-	query: []*pbUtils.AttributePredicate{
-		protoutils.AttributePredicate(protoutils.Attribute("attr", pbUtils.Attribute_S3TAGINT, nil),
-			protoutils.ValueInt(40),
-			protoutils.ValueInt(50),
-		),
-	},
-	assertObjectID: "object1",
+			false, false,
+		)
+		if err != nil {
+			t.Error(err)
+		}
+		streamRec, err := stream.Recv()
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, q.assertResponse, streamRec.GetLogOp().GetObjectId(), "")
+	}
 }
