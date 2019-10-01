ef66f3382ad9c89cf2babbfb64e4245d4bdd9bf1
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2019-10-01 17:10:03 +0200

    update Dockerfile

--- docker-proteus/latest/entrypoint.sh
+++ docker-proteus/latest/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 $GOPATH/src/github.com/dvasilas/proteus/docker-proteus/wait-for-it.sh --host=$1 --port=$2 --timeout=0
-$GOPATH/src/github.com/dvasilas/proteus/server -conf=$3
+go run $GOPATH/src/github.com/dvasilas/proteus/launcher/launcher.go $3 $4 $5 $6 $7
