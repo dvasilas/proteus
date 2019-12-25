8baf5ca6de768c1316bbaed97f75dddda82cd2a4
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2019-12-25 13:57:53 +0200

    java client: add notify query mode

--- proteus_java_client/src/main/java/io/grpc/proteusclient/ProteusClient.java
+++ proteus_java_client/src/main/java/io/grpc/proteusclient/ProteusClient.java
@@ -27,11 +27,18 @@ public class ProteusClient {
     channel.shutdown().shutdownNow();
   }
 
-  public void query(QueryPredicate []predicates, Map<String, String> metadata, CountDownLatch finishLatch, final StreamObserver<ResponseStreamRecord> requestObserver) {
-    SnapshotTimePredicate clock = SnapshotTimePredicate.newBuilder()
-      .setLbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.LATEST).build())
-      .setUbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.LATEST).build())
-      .build();
+  public void query(QueryPredicate []predicates, Map<String, String> metadata, CountDownLatch finishLatch, final StreamObserver<ResponseStreamRecord> requestObserver, Boolean notify) {
+    SnapshotTimePredicate clock;
+    if (!notify) {
+      clock = SnapshotTimePredicate.newBuilder()
+        .setLbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.LATEST).build())
+        .setUbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.LATEST).build())
+        .build();
+    } else {
+      clock = SnapshotTimePredicate.newBuilder()
+        .setLbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.INF).build())
+        .setUbound(SnapshotTime.newBuilder().setType(SnapshotTime.SnapshotTimeType.INF).build()).build();
+    }
 
     QueryRequest.Builder builder = QueryRequest.newBuilder().setClock(clock);
     for (int i=0; i<predicates.length; i++) {
@@ -76,7 +83,11 @@ public class ProteusClient {
     StreamObserver<RequestStream> toServer = asyncStub.query(
       new StreamObserver<ResponseStreamRecord>() {
       @Override
-      public void onNext(ResponseStreamRecord record) { requestObserver.onNext(record); }
+      public void onNext(ResponseStreamRecord record) {
+        if (record.getType() != ResponseStreamRecord.StreamRecordType.HEARTBEAT) {
+          requestObserver.onNext(record);
+        }
+      }
       @Override
       public void onError(Throwable t) { requestObserver.onError(t); }
       @Override
