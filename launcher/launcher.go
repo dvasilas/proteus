5cd15de801ddb2458ad27eae6c4c567c7cceaed3
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2020-05-02 09:35:24 +0200

    migrate to TOML configuration

--- launcher/launcher.go
+++ launcher/launcher.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
-	"path/filepath"
-	"runtime"
+	"io/ioutil"
 
 	"github.com/dvasilas/proteus/src/config"
 	"github.com/dvasilas/proteus/src/qpu/server"
+	"github.com/pelletier/go-toml"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	initDebug()
-
 	var configFile string
 	flag.StringVar(&configFile, "config", "noArg", "configuration file")
 	debug := flag.Bool("d", false, "show debug messages")
@@ -27,12 +22,11 @@ func main() {
 		log.SetLevel(log.InfoLevel)
 	}
 
-	var conf config.ConfJSON
+	conf := config.ConfJSON{}
 	err := readConfigFile(configFile, &conf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("config read from file", conf)
 
 	err = server.Server(conf)
 	if err != nil {
@@ -42,34 +36,13 @@ func main() {
 	}
 }
 
-//func readConfigFile(file string, conf *DSQPUConfig) error {
-func readConfigFile(file string, conf interface{}) error {
-	_, f, _, _ := runtime.Caller(0)
-	basepath := filepath.Dir(f)
-	viper.SetConfigName(file)
-	viper.AddConfigPath(basepath + "/")
-	viper.AddConfigPath(basepath + "/local")
-	viper.AddConfigPath(basepath + "/dockerCompose")
-	viper.SetConfigType("json")
-	if err := viper.ReadInConfig(); err != nil {
-		return err
-	}
-	if err := viper.Unmarshal(conf); err != nil {
-		return err
-	}
-	return nil
-}
-
-func initDebug() error {
-	err := viper.BindEnv("DEBUG")
+func readConfigFile(configFile string, conf *config.ConfJSON) error {
+	configData, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		return errors.New("BindEnv DEBUG failed")
+		log.Fatal(err)
 	}
-	debug := viper.GetBool("DEBUG")
-	if debug {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
-	}
+
+	toml.Unmarshal(configData, conf)
+
 	return nil
 }
