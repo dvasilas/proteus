d3a65d3eeab9d2b34b704149ca9d84008303bd5c
Dimitrios Vasilas <dv.vasilas@gmail.com>
2020-09-06 23:05:06 +0200

    set up datastore-plain correctly

--- build/datastore/lobsters-MySQL/docker-entrypoint-init.d/setup-proteus.sh
+++ build/datastore/lobsters-MySQL/docker-entrypoint-init.d/setup-proteus.sh
@@ -7,7 +7,7 @@ echo "Creating schema ..."
 mysql -uroot -p$MYSQL_ROOT_PASSWORD < /opt/proteus-lobsters/schema-trigger.sql
 
 echo "Loading data ..."
-mysql -uroot -p$MYSQL_ROOT_PASSWORD proteus_lobsters_db < /opt/proteus-lobsters/small.sql
+mysql -uroot -p$MYSQL_ROOT_PASSWORD proteus_lobsters_db < /opt/proteus-lobsters/small-proteus.sql
 
 echo "Running server ..."
 /opt/proteus-lobsters/server > /opt/proteus-lobsters/server.log &
