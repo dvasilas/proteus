f45fe23f79d5888173ec2637d8dd98e5545b0ce2
Dimitrios Vasilas <dv.vasilas@gmail.com>
2020-11-04 18:20:51 +0100

    add med size lobsters dataset

--- build/datastore/lobsters-MySQL/docker-entrypoint-init.d/setup-proteus.sh
+++ build/datastore/lobsters-MySQL/docker-entrypoint-init.d/setup-proteus.sh
@@ -8,6 +8,7 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD < /opt/proteus-lobsters/schema-trigger.sql
 
 echo "Loading data ..."
 mysql -uroot -p$MYSQL_ROOT_PASSWORD proteus_lobsters_db < /opt/proteus-lobsters/small.sql
+# mysql -uroot -p$MYSQL_ROOT_PASSWORD proteus_lobsters_db < /opt/proteus-lobsters/med.sql
 
 echo "Running server ..."
 /opt/proteus-lobsters/server > /opt/proteus-lobsters/server.log &
