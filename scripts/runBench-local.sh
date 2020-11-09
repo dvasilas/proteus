0d28bb217c966599ecdbe7d10043be99c438970d
Dimitrios Vasilas <dv.vasilas@gmail.com>
2020-11-09 14:03:40 +0100

    fixes and improvements in bench script

--- scripts/runBench-local.sh
+++ scripts/runBench-local.sh
@@ -3,11 +3,13 @@
 set -ex
 
 . runBench.functions 
+. runBench.config 
 
 PROTEUS_DIR=$1
 TAG=$2
 SHEET_ID=$3
 ROW=$4
+SYSTEM=$5
 
 export CREDENTIAL_FILE=
 export SPREADSHEET_ID=1OeeBC0vOM5qecenPMiiYH-2zCmaWz9BmJVEvCgtHnVc
@@ -16,11 +18,13 @@ export SHEET_ID=$SHEET_ID
 
 docker stack rm qpu-graph
 docker stack rm datastore-proteus
+docker stack rm datastore-plain
+docker stack rm datastore-mv
 
-pull
+#pul
 build
 
-sync proteus04 proteus-eu02 proteus-eu03 proteus-na-02
+sync proteus-eu02 proteus-eu03 proteus-eu04 #proteus-na01 proteus-na02
 
 docker network create -d overlay --attachable proteus_net || true
 
@@ -35,33 +39,42 @@ ROW=$((ROW+1))
 ./format-and-import.py -r $ROW --desc template-run
 ROW=$((ROW+1))
 
-#for i in "500 32 1" "1000 32 1" "1500 32 1" "2000 16 2" "2500 64 2" "2500 16 4" "2500 32 4" "3000 128 1" "3000 64 2" "3000 64 4" "3500 256 1"
-#for i in "2500 1024 1" "2500 2048 1"
-for i in "3500 1024 1" "3500 512 2"
+for ((i = 0; i < ${#runParams[@]}; i++))
 do
-	set -- $i
 	
-	echo "Timestamp: $(timestamp)" > /tmp/$1_$2_$3.out
+	set -- ${runParams[$i]}
+
 	logfile=$(timestamp_filename).txt
+	echo "Timestamp: $(timestamp)" > /tmp/$logfile
 	touch /tmp/$logfile
-	echo "Logfile: $logfile" >> /tmp/$1_$2_$3.out
 
-	env TAG_DATASTORE=$TAG docker stack deploy --compose-file $PROTEUS_DIR/deployments/compose-files/lobsters-benchmarks/datastore-proteus.yml datastore-proteus
+	if [ $SYSTEM == "mysql" ] ; then
+		env TAG_DATASTORE=$TAG docker stack deploy --compose-file $PROTEUS_DIR/deployments/compose-files/lobsters-benchmarks/datastore-mv.yml datastore-mv
+	fi
+	if [ $SYSTEM == "proteus" ] ; then
+		env TAG_DATASTORE=$TAG docker stack deploy --compose-file $PROTEUS_DIR/deployments/compose-files/lobsters-benchmarks/datastore-proteus.yml datastore-proteus
+	fi
+
 	wait_services_running
-
 	sleep 10
 
-	env TAG_QPU=$TAG docker stack deploy --compose-file $PROTEUS_DIR/deployments/compose-files/lobsters-benchmarks/qpu-graph.yml qpu-graph
-	wait_services_running
+	if [ $SYSTEM == "proteus" ] ; then
+		env TAG_DATASTORE=$TAG env TAG_QPU=$TAG docker stack deploy --compose-file $PROTEUS_DIR/deployments/compose-files/lobsters-benchmarks/qpu-graph.yml qpu-graph
+		wait_services_running
+		sleep 10
+	fi
 
 	$PROTEUS_DIR/scripts/utils/getPlacement.sh >> /tmp/$logfile
 
-	sleep 10
+	if [ $SYSTEM == "mysql" ] ; then
+		$PROTEUS_DIR/pkg/lobsters-bench/bin/benchmark -c $PROTEUS_DIR/pkg/lobsters-bench/config/config-mysql.toml -l $1 --fr $2 --fw $3 -t $4 >> /tmp/$logfile
+	fi
+	if [ $SYSTEM == "proteus" ] ; then
+		$PROTEUS_DIR/pkg/lobsters-bench/bin/benchmark -c $PROTEUS_DIR/pkg/lobsters-bench/config/config-proteus.toml -l $1 --fr $2 --fw $3 -t $4 >> /tmp/$logfile
+	fi
 
-	$PROTEUS_DIR/pkg/lobsters-bench/bin/benchmark -c $PROTEUS_DIR/pkg/lobsters-bench/config/config.toml -l $1 -f $2 -t $3 >> /tmp/$1_$2_$3.out
-
-	curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T /tmp/$logfile https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile
-	./format-and-import.py -r $ROW template-run /tmp/$1_$2_$3.out
+	./format-and-import.py -r $ROW template-run /tmp/$logfile
+#	curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T /tmp/$logfile https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile
 
 	docker stack rm qpu-graph
 	docker stack rm datastore-proteus
