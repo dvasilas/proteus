718e88d72c1763504f238b087ca7b7fc40d076f2
Dimitrios Vasilas <dv.vasilas@gmail.com>
2021-01-22 23:08:14 +0100

    java client: add connection pool

--- pkg/proteus-java-client/src/main/java/io/grpc/proteusclient/ProteusClient.java
+++ pkg/proteus-java-client/src/main/java/io/grpc/proteusclient/ProteusClient.java
@@ -4,28 +4,68 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.proteusclient.QPUAPIGrpc.QPUAPIBlockingStub;
 import io.grpc.stub.StreamObserver;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public final class ProteusClient {
-  private final ManagedChannel channel;
-  private final QPUAPIGrpc.QPUAPIBlockingStub stub;
+  private static int maxPoolSize;
+  private int initialPoolSize;
+  private int currentPoolSize;
+  private String host;
+  private int port;
 
-  public ProteusClient(String host, int port) {
-    this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+  private BlockingQueue<Connection> pool;
+
+  public ProteusClient(int initialPoolSize, int maxPoolSize, String host, int port) {
+    this.maxPoolSize = maxPoolSize > 0 ? maxPoolSize : 10;
+    this.initialPoolSize = initialPoolSize;
+    this.host = host;
+    this.port = port;
+
+    this.pool = new LinkedBlockingQueue<Connection>(maxPoolSize);
+
+    for (int i = 0; i < initialPoolSize; i++) {
+      openAndPoolConnection();
+    }
   }
 
-  public ProteusClient(ManagedChannelBuilder<?> channelBuilder) {
-    channel = channelBuilder.build();
-    stub = QPUAPIGrpc.newBlockingStub(channel);
+  private synchronized void openAndPoolConnection()  {
+    if (currentPoolSize == maxPoolSize) {
+      return;
+    }
+
+    pool.offer(new Connection(host, port));
+    currentPoolSize++;
+  }
+
+  private Connection getConnection() throws InterruptedException {
+    if (pool.peek()==null && currentPoolSize < maxPoolSize) {
+      openAndPoolConnection();
+    }
+
+  return pool.take();
+}
+
+  public void releaseConnection(Connection connection) {
+    pool.offer(connection);
   }
 
   public void shutdown() throws InterruptedException {
-    channel.shutdown().shutdownNow();
+    for (int i = 0; i < currentPoolSize; i++) {
+      Connection connection = pool.take();
+      connection.shutdown();
+    }
   }
 
-  public QueryResp query(java.lang.String queryStr) {
-    return stub.queryUnary(QueryReq.newBuilder().setQueryStr(queryStr).build());
+  public QueryResp query(java.lang.String queryStr) throws InterruptedException {
+    Connection connection = getConnection();
+    QueryResp resp = connection.query(queryStr);
+    releaseConnection(connection);
+    return resp;
   }
 }
